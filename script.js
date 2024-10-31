967c8ee063d61d0e3fc8b7cc424d9d64101fb779
ferianda1928@gmail.com <ferianda1928@gmail.com>
2024-10-31 21:39:07 +0700

    first task

---
+++ script.js
@@ -0,0 +1,4 @@
+// You can add some functionality here, for example, adding items to the cart, or slider functionality for the banner.
+document.querySelector('.banner-text button').addEventListener('click', function() {
+    alert('Redirecting to Shop Now page...');
+});
